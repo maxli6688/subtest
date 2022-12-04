ed272988c712f1a1a9f93061ea48edae9f86c0e4
enjoyali <2810539286@qq.com>
2022-12-04 10:56:50 +0800

    fix: add space graph

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,8 +1,29 @@
-import { NewGravatar, UpdatedGravatar } from '../generated/Gravity/Gravity'
-import { Gravatar } from '../generated/schema'
+// import { NewGravatar, UpdatedGravatar } from '../generated/Gravity/Gravity'
+// import { Gravatar } from '../generated/schema'
+// export function handleNewGravatar(event: NewGravatar): void {
+//   let gravatar = new Gravatar(event.params.id.toHex())
+//   gravatar.owner = event.params.owner
+//   gravatar.displayName = event.params.displayName
+//   gravatar.imageUrl = event.params.imageUrl
+//   gravatar.save()
+// }
+
+// export function handleUpdatedGravatar(event: UpdatedGravatar): void {
+//   let id = event.params.id.toHex()
+//   let gravatar = Gravatar.load(id)
+//   if (gravatar == null) {
+//     gravatar = new Gravatar(id)
+//   }
+//   gravatar.owner = event.params.owner
+//   gravatar.displayName = event.params.displayName
+//   gravatar.imageUrl = event.params.imageUrl
+//   gravatar.save()
+// }
+import { NewGravatar, UpdatedGravatar } from '../generated/Space/Space'
+import { Space } from '../generated/schema'
 
 export function handleNewGravatar(event: NewGravatar): void {
-  let gravatar = new Gravatar(event.params.id.toHex())
+  let gravatar = new Space(event.params.id.toHex())
   gravatar.owner = event.params.owner
   gravatar.displayName = event.params.displayName
   gravatar.imageUrl = event.params.imageUrl
@@ -11,9 +32,9 @@ export function handleNewGravatar(event: NewGravatar): void {
 
 export function handleUpdatedGravatar(event: UpdatedGravatar): void {
   let id = event.params.id.toHex()
-  let gravatar = Gravatar.load(id)
+  let gravatar = Space.load(id)
   if (gravatar == null) {
-    gravatar = new Gravatar(id)
+    gravatar = new Space(id)
   }
   gravatar.owner = event.params.owner
   gravatar.displayName = event.params.displayName
