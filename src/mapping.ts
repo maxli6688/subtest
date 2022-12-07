3981d2e52a52da97f40261f07317fa0ba853e92e
enjoyali <2810539286@qq.com>
2022-12-08 00:09:58 +0800

    fix: spacemanager

--- src/mapping.ts
+++ src/mapping.ts
@@ -19,8 +19,11 @@
 //   gravatar.imageUrl = event.params.imageUrl
 //   gravatar.save()
 // }
+import { ipfs, json, JSONValueKind } from '@graphprotocol/graph-ts'
+
 import { NewGravatar, UpdatedGravatar } from '../generated/Space/Space'
-import { Space } from '../generated/schema'
+import { NewSpaceRegistered } from '../generated/SpaceManager/SpaceManager'
+import { Space, SpaceManager } from '../generated/schema'
 
 export function handleNewGravatar(event: NewGravatar): void {
   let gravatar = new Space(event.params.id.toHex())
@@ -40,4 +43,30 @@ export function handleUpdatedGravatar(event: UpdatedGravatar): void {
   gravatar.displayName = event.params.displayName
   gravatar.imageUrl = event.params.imageUrl
   gravatar.save()
+}
+
+export function handleNewSpaceRegistered(event: NewSpaceRegistered): void {
+  let id = event.params.newSpaceId.toHex()
+  let space = SpaceManager.load(id)
+  if (space == null) {
+    space = new SpaceManager(id)
+  }
+  space.owner = event.params.registerAddress
+  space.metadataUrl = event.params.detail
+  const path = space.metadataUrl.replace('ipfs://', '')
+  const data = ipfs.cat(path)
+  // space.name = event.params.imageUrl
+  if (data) {
+    const res = json.fromBytes(data)
+    if (res.kind === JSONValueKind.OBJECT) {
+      const obj = res.toObject()
+      if (obj.get('name')) {
+        space.name = obj.get('name')!.toString()
+      }
+      if (obj.get('description')) {
+        space.description = obj.get('description')!.toString()
+      }
+    }
+  }
+  space.save()
 }
